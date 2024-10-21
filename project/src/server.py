804eda5351f9a2415ee7506e254f3f00d151a057
Nithin Dhananjayan <ndhananj@users.noreply.github.com>
2024-10-21 02:05:34 +0000

    Fix internal server error on model serving

--- project/src/server.py
+++ project/src/server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from ray import serve
 from ray.serve.handle import DeploymentHandle
-from loguru import logger
 
 from src.data_models import SimpleModelRequest, SimpleModelResponse, SimpleModelResults
 from src.model import Model
@@ -14,7 +13,6 @@ app = FastAPI(
 
 # TODO: Add in appropriate logging using loguru wherever you see fit
 # in order to aid with debugging issues.
-logger.add("app.log", rotation="500 MB")
 
 @serve.deployment(
     ray_actor_options={"num_cpus": 0.2},
@@ -29,9 +27,9 @@ class APIIngress:
     async def predict(self, request: SimpleModelRequest):
         # DONE: Use the handle.predict which is a remote function
         # to get the result
-        logger.info(f"Received prediction request for review: {request.review[:50]}...")
+        #logger.info(f"Received prediction request for review: {request.review[:50]}...")
         result = await self.handle.predict.remote(request.review)
-        logger.info("Prediction completed successfully")
+        #logger.info("Prediction completed successfully")
         return SimpleModelResponse.model_validate(result.model_dump())
 
 
@@ -45,9 +43,7 @@ class SimpleModel:
 
     def predict(self, review: str) -> SimpleModelResults:
         # DONE: Use the Model.predict to get the result
-        logger.info(f"Predicting sentiment for review: {review[:50]}...")
-        result = self.session.predict(review)
-        logger.info("Prediction completed")
+        result = Model.predict(self.session, review)
         return SimpleModelResults.model_validate(result)
 
 
